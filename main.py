ed86ea67d3b70b78924c877aef26c2687ba83057
Ovchinnikov Sergei <owchin.s@yandex.ru>
2025-05-29 11:57:51 +0300

    Отображение названия конфига

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     print(f"Config name: {args.config}")
+
     emulator = GameEmulator(f'config/{args.config}')
     emulator.play_game()
 
